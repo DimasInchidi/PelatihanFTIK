b9d2f9b12a0c061720c3b58885e8a2d7a7ba8ab2
DimasInchidi <Inchidi@yahoo.com>
2015-12-05 06:33:56 +0800

    update ya kan

--- src/main/java/servlet/MainTestMenu.java
+++ src/main/java/servlet/MainTestMenu.java
@@ -5,10 +5,13 @@ import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@WebServlet(
+        name = "LoginFB",
+        urlPatterns = {"/Facebook_Login/fbhome"}    )
 public class MainTestMenu extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
